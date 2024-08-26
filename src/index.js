65d8e24ee940a0ec553a635752316d7a11e03815
Rocky <rockytan@users.noreply.github.com>
2024-08-26 18:15:00 +0800

    修改域名

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -7,17 +9,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.m.leyan.cc": dockerHub,
+  "quay.m.leyan.cc": "https://quay.io",
+  "gcr.m.leyan.cc": "https://gcr.io",
+  "k8s-gcr.m.leyan.cc": "https://k8s.gcr.io",
+  "k8s.m.leyan.cc": "https://registry.k8s.io",
+  "ghcr.m.leyan.cc": "https://ghcr.io",
+  "cloudsmith.m.leyan.cc": "https://docker.cloudsmith.io",
+  "ecr.m.leyan.cc": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.m.leyan.cc": dockerHub,
 };
 
 function routeByHosts(host) {
@@ -43,6 +45,16 @@ async function handleRequest(request) {
       }
     );
   }
+
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
+  }
+
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
   if (url.pathname == "/v2/") {
